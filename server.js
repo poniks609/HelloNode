e27aca109480e6ccae1e0bb1efe25f2d4da771bc
poniks609 <haruto2010haba@gmail.com>
2025-10-10 05:21:38 +0000

    one step fuerder

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var exec = require("child_process").exec;
+const fetch = require('node-fetch');
+var content = "err";
+fetch('http://quotes.toscrape.com')
+  .then(response => response.text())
+  .then(body => content = body)
+  .catch(err => content = err);
+
 var http = require("http");
 
 http.createServer(function(request,response){
     response.writeHead(200, {"Content-Type":"text/html"});
-    var content = "HelloNode";
-    exec("curl -o arg https://example.com",function(arg){content = arg;});
     response.write(
         `<head><meta http-equiv="Content-Type" content="text/html;charset=UFT-8" /></head>`+
         `<body><h>${content}</h><input type="button" value="Exec" onclick="OnButtonClick();"/></body>`
