6c1fb93aa67a540019bdb1739a19e18f772f5967
poniks609 <haruto2010haba@gmail.com>
2025-10-10 05:14:02 +0000

    downloded node-fetch

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+var exec = require("child_process").exec;
 var http = require("http");
 
 http.createServer(function(request,response){
     response.writeHead(200, {"Content-Type":"text/html"});
+    var content = "HelloNode";
+    exec("curl -o arg https://example.com",function(arg){content = arg;});
     response.write(
-        '<head><meta http-equiv="Content-Type" content="text/html;charset=UFT-8" /></head>'+
-        '<body><h>HelloNode</h><botton></botton></body>')
+        `<head><meta http-equiv="Content-Type" content="text/html;charset=UFT-8" /></head>`+
+        `<body><h>${content}</h><input type="button" value="Exec" onclick="OnButtonClick();"/></body>`
+    );
+    console.log("conected");
     response.end();
 }).listen(8888);
