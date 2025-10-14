1992cdd943fe255d46db0d38e3fee7ca894bd8ef
poniks609 <haruto2010haba@gmail.com>
2025-10-14 04:09:03 +0000

    new package

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const fetch = require('node-fetch');
 var content = "err";
-fetch('http://quotes.toscrape.com')
+/*fetch('http://quotes.toscrape.com')
   .then(response => response.text())
   .then(body => content = body)
   .catch(err => content = err);
-
+*/
 var http = require("http");
 
+function OnButtonClick (){
+    fetch('http://quotes.toscrape.com')
+        .then(response => response.text())
+        .then(body => content = body)
+        .catch(err => content = err);
+}
+console.log("server started");
 http.createServer(function(request,response){
+    function OnButtonClick(){
+        fetch('http://quotes.toscrape.com')
+            .then(response => response.text())
+            .then(body => content = body)
+            .catch(err => content = err);
+    }
     response.writeHead(200, {"Content-Type":"text/html"});
     response.write(
         `<head><meta http-equiv="Content-Type" content="text/html;charset=UFT-8" /></head>`+
-        `<body><h>${content}</h><input type="button" value="Exec" onclick="OnButtonClick();"/></body>`
+        `<body>${content}<input type="button" value="Exec" onclick="OnButtonClick();"/></body>`
     );
     console.log("conected");
     response.end();
